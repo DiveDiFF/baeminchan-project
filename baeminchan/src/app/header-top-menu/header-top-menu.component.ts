d403b074c31970594b8ca52874f53e5e3ed27442
silveruno <silveruno123@gmail.com>
2018-07-30 17:22:35 +0900

    Edit header component CSS 스타일링 진행중

---
+++ baeminchan/src/app/header-top-menu/header-top-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-top-menu',
+  templateUrl: './header-top-menu.component.html',
+  styleUrls: ['./header-top-menu.component.css']
+})
+export class HeaderTopMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
